38134a0c6587279f4314124230029677d814eff9
vaishnavikotwal-droid <vaishnavikotwal17@gmail.com>
2026-09-21 20:37:15 +0530

    refactor: consolidate emergency contact functions in alerts.js for better organization

--- js/alerts.js
+++ js/alerts.js
@@ -23,4 +23,36 @@ function callFire() {
 
     alert("Fire Emergency Number: 101");
 
-}
+    function refreshAlerts() {
+        alert("Traffic alerts refreshed successfully!");
+    }
+
+    function callPolice() {
+        alert("Police Emergency Number: 112");
+    }
+
+    function callAmbulance() {
+        alert("Ambulance Emergency Number: 108");
+    }
+
+    function callFire() {
+        alert("Fire Brigade Emergency Number: 101");
+    }
+
+    function callHighway() {
+        alert("Highway Emergency Number: 1033");
+    }
+
+    function callWomen() {
+        alert("Women Helpline Number: 181");
+    }
+
+    function callChild() {
+        alert("Child Helpline Number: 1098");
+    }
+
+    function callDisaster() {
+        alert("Disaster Management Number: 1077");
+
+
+    }
