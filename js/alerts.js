b66e8c653921dee4e9d2bb46d78b35759f6cf3ff
vaishnavikotwal-droid <vaishnavikotwal17@gmail.com>
2026-09-21 15:15:39 +0530

    feat: implement traffic alerts page with structured layout and emergency contact functions

---
+++ js/alerts.js
@@ -0,0 +1,26 @@
+function refreshAlerts() {
+
+    alert("Traffic alerts refreshed successfully!");
+
+}
+
+
+function callPolice() {
+
+    alert("Police Emergency Number: 112");
+
+}
+
+
+function callAmbulance() {
+
+    alert("Ambulance Emergency Number: 108");
+
+}
+
+
+function callFire() {
+
+    alert("Fire Emergency Number: 101");
+
+}
